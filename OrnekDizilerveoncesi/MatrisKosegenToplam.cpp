af0fb4be46f4be6cfd617c37855f80eb02c2d0f8
SteelM25 <cmusa4031@gmail.com>
2025-11-06 15:27:21 +0300

    soru

---
+++ OrnekDizilerveoncesi/MatrisKosegenToplam.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*Kullanıcıdan 4x4 boyutunda bir matris alın.
+
+* Ana köşegen (sol üst → sağ alt) ve yan köşegen (sağ üst → sol alt) elemanlarının toplamını bulun.
+* Bu iki toplamı karşılaştırın: hangisi büyükse ekrana belirtin.
+  Örnek:
+
+```
+Ana köşegen toplamı: 45
+Yan köşegen toplamı: 50
+Sonuç: Yan köşegen daha büyük.*/
+
+int main(){
+
+    
+
+}
