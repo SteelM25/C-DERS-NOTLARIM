680a50073e66e65e251e21f27aee1c6485516b01
SteelM25 <cmusa4031@gmail.com>
2025-11-06 13:56:12 +0300

    soruyu yazdık

---
+++ OrnekDizilerveoncesi/FonksiyonlaOrtakElemanBulma.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*İki farklı diziyi (5 elemanlı) kullanıcıdan alın.
+Bir fonksiyon tanımlayın:
+
+```cpp
+void ortakElemanlar(int d1[], int d2[], int boyut);
+```
+
+Fonksiyon, her iki dizide de bulunan ortak elemanları ekrana yazdırsın.
+Eğer hiç ortak eleman yoksa “Ortak eleman yok” yazsın.*/
+
+
+int main(){
+
+    
+
+}
