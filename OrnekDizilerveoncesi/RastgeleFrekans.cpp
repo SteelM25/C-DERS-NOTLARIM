fe6408385510e8c36269415fd3e5a2ee0a77274b
SteelM25 <cmusa4031@gmail.com>
2025-11-06 13:52:23 +0300

    .

--- OrnekDizilerveoncesi/RastgeleFrekans.cpp
+++ OrnekDizilerveoncesi/RastgeleFrekans.cpp
@@ -2,14 +2,8 @@
 using namespace std;
 
 /*`rand()` fonksiyonuyla **0–9** arasında 100 rastgele sayı üretin.
-Bir dizide bu sayıların frekansını tutun.
-Yani:
+bu sayıların frekansını tutun.
 
-```cpp
-frekans[0] → 0 kaç kez geldi
-frekans[1] → 1 kaç kez geldi
-...
-```
 
 En sık geçen sayıyı ve kaç kez tekrarlandığını bulun.*/
 
