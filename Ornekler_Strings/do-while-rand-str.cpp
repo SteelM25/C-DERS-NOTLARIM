fb2c172c1d9ce355fdd552e643cfb52be8b8df8d
SteelM25 <cmusa4031@gmail.com>
2025-11-10 17:08:13 +0300

    tamam

---
+++ Ornekler_Strings/do-while-rand-str.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*Bir “rastgele harf üretme oyunu” yaz:
+
+rand() fonksiyonuyla a–z arası rastgele bir karakter üret. ('a' ve 'z' karakterlerinin ASCII değerleri sırasıyla 97 ve 122’dir.)
+
+Kullanıcıdan tahmin al.
+
+Tahmin doğru olana kadar do-while döngüsüyle devam et.
+
+Doğru tahmin edildiğinde “Tebrikler!” yazdır.*/
+
+int main(){
+    
+    srand(time(0));
+    
+    char harf = 'a' + rand() % 26;
+    
+    char tahmin;
+    
+    do {
+        cout << "\n Bir harf tahmin ediniz: ";
+        cin >> tahmin;
+    }while (harf != tahmin);
+
+    cout << "Eşleşen harf: " << harf; 
+
+
+}
