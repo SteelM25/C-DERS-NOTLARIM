4cb88bf0ebe2a08c5617ae0c5f66c95e8d960889
SteelM25 <cmusa4031@gmail.com>
2025-11-13 00:30:27 +0300

    const

---
+++ const.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //* const ile atanan değişkenin değeri başka yerde değiştirilemez. Ayrıca const ile değişken atanacağı zaman tanımlama ve atama tek seferde yapılır, diğerleri gibi tanımlama yapılıp atama daha sonraya bırakılamaz.
+
+    const double pi = 3.14;
+
+    // pi = 3.142     OLMAZ
+    
+
+
+}
