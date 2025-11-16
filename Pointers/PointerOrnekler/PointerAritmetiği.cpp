0e19621c0b7f0148e7ada96977a2073693ace1bd
SteelM25 <cmusa4031@gmail.com>
2025-11-16 23:24:53 +0300

    ...

---
+++ Pointers/PointerOrnekler/PointerAritmetiği.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+//? Bir int pointer tanımla. Pointer’ın tuttuğu adresi ve ptr + 1 ile oluşan adresi ekrana yazdırarak aradaki farkı gözlemle.
+
+int main(){
+    
+}
