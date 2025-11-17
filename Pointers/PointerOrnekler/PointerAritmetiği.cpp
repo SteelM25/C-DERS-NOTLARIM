9ef0151ef1aefcde31fd56afebf823fc2853a191
SteelM25 <cmusa4031@gmail.com>
2025-11-17 21:55:44 +0300

    tamam

--- Pointers/PointerOrnekler/PointerAritmetiği.cpp
+++ Pointers/PointerOrnekler/PointerAritmetiği.cpp
@@ -4,5 +4,12 @@ using namespace std;
 //? Bir int pointer tanımla. Pointer’ın tuttuğu adresi ve ptr + 1 ile oluşan adresi ekrana yazdırarak aradaki farkı gözlemle.
 
 int main(){
+
+    int* sayi;
+
+    cout << endl << sayi << endl;
+    cout << (sayi + 1) << endl;
+
+
     
 }
