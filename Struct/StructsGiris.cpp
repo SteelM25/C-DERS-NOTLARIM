7ee351eedefb4b637fc4f42e040d08da4bef4613
SteelM25 <cmusa4031@gmail.com>
2025-11-20 10:28:21 +0300

    Bazı tanımlar eklendi

---
+++ Struct/StructsGiris.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+//? structs konusunu 2 kısımda inceleyeceğiz. 1. kısmı C tipi struct (burada typedef keywordü yok/ normalde C'de typedef kullanılır ama C++ dilinde buna gerek yok. Yani 1. kısımda C tipi struct göreceğiz ama typedef olmadan...), 2. kısım C++ dilindeki. 2. kısmı OOP kısmından sonra göreceğiz.
+
+int main(){
+
+    //Struct; kelime anlamı "Yapı". İngilizce structure kelimesinden gelir.
+
+    //? struct, ilişkili verileri tek bir çatı altında toplar. Yani birbirinden farklı veri tipine sahip ama birbiriyle bağlantılı verileri tek bir bölümde tutar. Mesela öğrenci bilgilerini içeren bir struct int tipinde öğrenci no, double tipinde notları, string tipinde isimleri vs. tek bir çatı altında class mantığıyla tutabilir.
+
+    // struct içindeki her bir değişkene "üye" (member) denir.
+
+    // struct, C++'ta "veri kümeleri" oluşturmak için en temel yapılardan biridir.
+
+
+
+
+}
