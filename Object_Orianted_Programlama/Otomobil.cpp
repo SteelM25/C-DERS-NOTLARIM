326edb0ead5c2402a015ba8a96706b3f1534aa96
SteelM25 <cmusa4031@gmail.com>
2025-11-27 12:55:10 +0300

    .

--- Object_Orianted_Programlama/Otomobil.cpp
+++ Object_Orianted_Programlama/Otomobil.cpp
@@ -2,6 +2,7 @@
 // sınıfı bağladık.
 
 //?Burası isimlendirilen içerikleri doldurma kısmı
+//Eğer metod class dışında tanımlanırsa bu yöntem kullanılır.
 void Otomobil::ruhsatBilgileri(){
     cout << "Model: " << Otomobil::model << endl;
     cout << "Renk: " << Otomobil::renk << endl;
