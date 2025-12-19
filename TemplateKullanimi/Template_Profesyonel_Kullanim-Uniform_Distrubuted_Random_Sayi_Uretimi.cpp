615b537fe7ac88f400dd1c3c9ba2267f760517ec
SteelM25 <cmusa4031@gmail.com>
2025-12-19 21:05:12 +0300

    uniform_int_distribution giriş

---
+++ TemplateKullanimi/Template_Profesyonel_Kullanim-Uniform_Distrubuted_Random_Sayi_Uretimi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+    uniform_int_distribution sınıfını kullanarak random sayı üretme
+
+- rand() ile üretilen sayıların "dağılımı" tam düzgün değildir.
+- Yani bazen 2'ler çok çıkar, bazen 5 hiç çıkmaz.
+- uniform_int_distribution istatistiksel olarak gerçek eşit olasılıklı dağılım yapar.
+- Büyük projelerde (oyunlar, simülasyonlar, yapay zeka) mutlaka uniform_int_distribution kullanılır(profesyonel kullanım)
+
+*/
+
+int main(){
+
+
+
+
+}
--- TemplateKullanimi/main.cpp
+++ TemplateKullanimi/main.cpp
@@ -6,11 +6,56 @@ using namespace std;
 //Okey taşı gibi her taşın yerine geçer:)
 // Ama bir template değerleri aynı türden olmalı.
 
-
+//Template fonksiyon kullanımı
 template <typename T>
 T topla(T a, T b){
     return a + b;
 }
+
+//Çıkarma fonksiyonu
+template <typename T>
+T cikar(T x, T y){
+    return x - y;
+}
+
+//Template class kullanımı
+template <typename T>
+class Kare{
+private:
+    T kenar;
+public:
+    Kare(T uzunluk){
+        kenar = uzunluk;
+    }
+    T alan(){
+        return kenar * kenar;
+    }
+
+    void goster(){
+        cout << "Kenar uzunluk: " << kenar << endl;
+    }
+};
+
+//Ornek Dikdortgen sinifi
+template <typename T>
+class Dikdortgen{
+private:
+    T kisa;
+    T uzun;
+public:
+    Dikdortgen(T _kisa, T _uzun){
+        kisa = _kisa;
+        uzun = _uzun;
+    }
+    T alan(){
+        return kisa * uzun;
+    }
+
+    void goster(){
+        cout << "Kisa kenar: " << kisa << endl;
+        cout << "Uzun kenar: " << uzun << endl;
+    }
+};
 
 int main()
 {
@@ -18,5 +63,37 @@ int main()
     cout << topla(4.9, 9.3) << endl;
     cout << topla(4, 9) << endl;
     cout << topla(string("Merhaba ") , string("Musa")) << endl;
+
+    //Class kullanımında obje tipini önceden belirtmeliyiz.
+
+    Kare<int> k1(9); // int tipinde bir kare
+    Kare<double> k2(8.2); // double tipinde bir kare
+
+    k1.goster();
+    cout << "K1 alan: " << k1.alan() << endl;
+
+    k2.goster();
+    cout << "K2 alan: " << k2.alan() << endl;
+
+
+    //Örnek Dikdortgen sınıfı
+
+    Dikdortgen<int> a1(3,9);
+    Dikdortgen<double> a2(8.9, 2.4);
+
+    a1.goster();
+    cout << "A1 alan: " << a1.alan() << endl;
+
+    a2.goster();
+    cout << "A2 alan: " << a2.alan() << endl;
+
+    cout << "Çıkarma fonksiyonu int sonucu: " << cikar(9,4) << endl;
+
+    cout << "Çıkarma fonksiyonu double sonucu: " << cikar(3.2, 1.9) << endl;
+
+
+
+
+
 
 }
