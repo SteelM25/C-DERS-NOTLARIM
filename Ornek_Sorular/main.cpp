02de376394fe70226486a68e2be3f398706775cc
SteelM25 <cmusa4031@gmail.com>
2026-01-06 21:28:26 +0300

    2.yol eklendi

--- Ornek_Sorular/main.cpp
+++ Ornek_Sorular/main.cpp
@@ -33,17 +33,27 @@ void ciftKaresi(int a[], int uzunluk){
 int main()
 {
     //Dizi elemanlarını kullanıcıdan almak istiyorum.
+    // int boyut;
+
+    // cout << "Dizi kaç elemanlı olsun: ";
+    // cin >> boyut;
+    // int dizi[boyut];
+
+    // //Elemanları kullanıcıdan aldık.
+    // for(int i = 0; i < boyut; i++){
+    //     cout << "Dizinin " << i + 1 << ". elemanını giriniz: ";
+    //     cin >> dizi[i];
+    // }
+
+    // ciftKaresi(dizi,boyut);
+
+    //2. Yol
+
     int boyut;
 
-    cout << "Dizi kaç elemanlı olsun: ";
-    cin >> boyut;
-    int dizi[boyut];
+    int dizi[] = {12, 4, 3, 9, 8, 6};
 
-    //Elemanları kullanıcıdan aldık.
-    for(int i = 0; i < boyut; i++){
-        cout << "Dizinin " << i + 1 << ". elemanını giriniz: ";
-        cin >> dizi[i];
-    }
+    boyut = (sizeof(dizi) / sizeof(dizi[0])); // sizeof metodu int türündeki her bir değeri 4 byte olarak tutar. Yani 1 değer 4, 2 değer 8, 3 değer 12 olarak tutulur. Öyleyse bu metodu kullanıp sonucu 4'e bölersek int dizisinin uzunluğunu bulmuş oluruz.
 
     ciftKaresi(dizi,boyut);
 
