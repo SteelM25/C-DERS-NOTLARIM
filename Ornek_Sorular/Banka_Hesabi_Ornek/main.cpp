d6e67c715f3aba9b0e30e539cccc22049371c9c6
SteelM25 <cmusa4031@gmail.com>
2026-01-08 13:28:52 +0300

    Birkaç güncelleme

--- Ornek_Sorular/Banka_Hesabi_Ornek/main.cpp
+++ Ornek_Sorular/Banka_Hesabi_Ornek/main.cpp
@@ -11,31 +11,21 @@ public:
     //Varsayılan değerli constructor
     Hesap(string ad = "", int no = 0, float _bakiye = 0) : hesapAdi(ad), hesapNo(no), bakiye(_bakiye){}
 
-    void paraYatir(){
-        int yatir;
-        cout << hesapAdi << " hesabına yatırmak istediğiniz tutar: ";
-        cin >> yatir;
-
+    void paraYatir(int yatir){
         if(yatir > 0){
             bakiye += yatir;
-            cout << "Yeni bakiye: " << bakiye << endl;
         }
         else{
             cout << "Lütfen pozitif bir değer giriniz.." << endl;
         }
     }
 
-    void paraCek(){
-        int cek;
-        cout << hesapAdi << " hesabından çekmek istediğiniz tutar: ";
-        cin >> cek;
-
+    void paraCek(int cek){
         if(cek <= bakiye){
             bakiye -= cek;
-            cout << "Yeni bakiye: " << bakiye << endl;
         }
         else{
-            cout << "Lütfen geçerli bir değer giriniz...\n\n";
+            cout << "Yetersiz bakiye...\n";
         }
     }
 
@@ -50,10 +40,11 @@ int main()
     Hesap h1("Ali Kayahan",172123, 250.75);
     Hesap h2("Berna Ozan", 174153, 0);
 
-    h1.paraCek();
+    h1.paraYatir(200);
+    h1.paraCek(100);
     h1.bilgiYazdir();
 
-    h2.paraCek();
+    h2.paraCek(100);
     h2.bilgiYazdir();
 
     return 0;
